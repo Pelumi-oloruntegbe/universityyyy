7c2c96997eb71b9c804ded8fafb730c7cbe8c606
Pelumi Oloruntegbe <oloruntegbe.pelumi@gmail.com>
2024-01-02 19:47:55 +0100

    first commits

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import './App.css'
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state ={
+            robots: [],
+            searchfield: ''
+        }
+    }
+   
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users}));
+    }
+    
+
+
+    onsearchChange = (event) => {
+        this.setState({ searchfield: event.target.value})
+    }
+
+    render () {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !robots.length ?
+         <h1>Loading</h1> :
+         (
+                <div className='tc'>
+                <h1 className='f1'>RoboFriend</h1>
+                <SearchBox searchChange={this.onsearchChange}/>
+                <Scroll>
+                <CardList robots={filteredRobots} />
+                </Scroll>
+                </div>
+        );
+    
+    }
+}  
+
+export default App;
